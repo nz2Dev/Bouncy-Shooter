38684ad06a34a299f2539d152f0fb5ffa1eb0c01
Nazar Pinonzhyk <nz2develop@gmail.com>
2023-02-03 21:17:04 +0200

    Fix radius and sphere geometry scale syncing math

--- Assets/Scripts/ResizableSphere.cs
+++ Assets/Scripts/ResizableSphere.cs
@@ -32,7 +32,10 @@ public class ResizableSphere : MonoBehaviour {
     }
 
     private void SyncScaleWithRadius() {
-        geometry.transform.localScale = Vector3.one * radius;
+        // default unit sphere mesh with origin at the center will cover half of unit in the scene (in radius) when scale is one
+        // that's why we should multiply scale twise to represent the proper amount of units that float radius contains
+        // in other words: 1 radius = 2 * scale of half unit sphere
+        geometry.transform.localScale = Vector3.one * (radius * 2f);
     }
 
 }
