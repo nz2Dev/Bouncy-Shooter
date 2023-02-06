79707473d71b3b04c60de4c17ddafc8078ca9252
Nazar Pinonzhyk <nz2develop@gmail.com>
2023-02-06 22:50:58 +0200

    Remove setting start radius value for Ball

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -7,30 +7,27 @@ public class Ball : MonoBehaviour {
 
     public event Action OnRadiusChanged;
 
-    public float Radius => radius;
+    public float Radius => _radius;
 
-    [SerializeField] private float radius = 1f;
     [SerializeField] private float minRadius = 0.1f;
     [SerializeField] private float maxRadius = 5;
-
-    private void Start() {
-        SetRadius(radius);
-    }
+    
+    private float _radius = 1f;
 
     public void SetRadiusToMinimum() {
         SetRadius(minRadius);
     }
 
     public void SetRadius(float newRadius) {
-        radius = Mathf.Clamp(newRadius, minRadius, maxRadius);
+        _radius = Mathf.Clamp(newRadius, minRadius, maxRadius);
         OnRadiusChanged?.Invoke();
     }
 
     public bool ChangeRadius(float radiusDelta) {
-        var changesRadius = radius + radiusDelta;
-        radius = Mathf.Clamp(changesRadius, minRadius, maxRadius);
+        var changesRadius = _radius + radiusDelta;
+        _radius = Mathf.Clamp(changesRadius, minRadius, maxRadius);
         OnRadiusChanged?.Invoke();
-        return radius == changesRadius;
+        return _radius == changesRadius;
     }
 
 }
