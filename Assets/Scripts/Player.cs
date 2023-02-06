2c492475a2b954d15b659479eaf216d9cd4dfcc6
Nazar Pinonzhyk <nz2develop@gmail.com>
2023-02-06 23:32:07 +0200

    Set bullet position at the start

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -21,6 +21,9 @@ public class Player : MonoBehaviour, IBallShape {
         OnRadiusChanged?.Invoke();
 
         bullet.SetRadiusToMinimum();
+
+        var initialDistanceToBullet = startRadius + bulletOffset;
+        bullet.SetPosition(transform.position + transform.forward * initialDistanceToBullet);
     }
 
     private void Update() {
@@ -41,9 +44,6 @@ public class Player : MonoBehaviour, IBallShape {
         Charging = true;
 
         bullet.SetRadiusToMinimum();
-
-        var initialDistanceToBullet = Radius + bullet.Radius + bulletOffset;
-        bullet.SetPosition(transform.position + transform.forward * initialDistanceToBullet);
     }
 
     private void Charge() {
