917494d44d081386ad150037ca9ac338eb423d68
Nazar Pinonzhyk <nz2develop@gmail.com>
2023-02-07 20:28:47 +0200

    Add GameOverUI elements to the game scene

---
+++ Assets/Scripts/GameOverUI.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverUI : MonoBehaviour {
+
+    public void OnRestartButtonClicked() {
+        Debug.LogError("Restart not implemented");
+    }
+}
