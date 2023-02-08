ff50e4997e953eb2f1adc52a3b5fe4b225bfac03
Nazar Pinonzhyk <nz2develop@gmail.com>
2023-02-08 13:35:56 +0200

    Add OnPlayerEntered event for Door logic

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -6,18 +6,32 @@ using UnityEngine;
 public class Door : MonoBehaviour {
 
     public event Action OnOpened;
+    public event Action OnPlayerEntered;
 
     public bool IsOpen { get; private set; }
+    public bool IsPlayerEntered { get; private set; }
 
     [SerializeField] private Player player;
     [SerializeField] private float openDistance = 5f;
 
     private void Update() {
-        if (!IsOpen) {
+        if (!IsPlayerEntered) {
+            // player has not entered yet
             var distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
-            if (distanceToPlayer < openDistance) {
-                IsOpen = true;
-                OnOpened?.Invoke();
+
+            if (!IsOpen) {
+                // door is not opened yet
+                if (distanceToPlayer < openDistance) {
+                    IsOpen = true;
+                    OnOpened?.Invoke();
+                }
+            } else {
+                // door is opened now
+                if (distanceToPlayer < 1.0f) {
+                    // player has entered
+                    IsPlayerEntered = true;
+                    OnPlayerEntered?.Invoke();
+                }
             }
         }
     }
